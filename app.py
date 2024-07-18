614f403ed6bd34e06d804146692f17cdbeb9b8b8
Buse <94009706+Busedkc@users.noreply.github.com>
2024-07-18 10:18:05 +0300

    .

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ y = wine['quality'].apply(lambda yval:1 if yval >= 7 else 0)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X,y, test_size=0.2, random_state=3)
 #Train the Random Forest Classifier model
-model = RandomForesrClassifier()
+model = RandomForestClassifier()
 model.fit(X_train, Y_train)
 # accuracy on the test data
 X_test_prediction = model.predict(X_test)
